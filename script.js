fdbafc4f3a46c2cff5378b68db7d377b41dedf0f
KurtAstarita <itskurtastarita@gmail.com>
2025-04-30 20:50:06 -0400

    Update script.js

--- script.js
+++ script.js
@@ -24,26 +24,3 @@ const dropdown = document.querySelector('.dropdown');
       dropdownContent.style.display = 'none';
     }, 200); // Adjust the delay here as well if needed
   });
-
-<!-- .............................................................. -->
-
-
-
-window.addEventListener('message', function(event) {
-    // Check the origin of the message for security if you're not using '*'
-    // if (event.origin !== 'https://kurtastarita.github.io') return;
-
-    if (event.data && event.data.height) {
-        const iframe = document.getElementById('myWorkoutLogIframe');
-        if (iframe) {
-            iframe.style.height = event.data.height + 'px';
-        }
-    }
-});
-
-window.onload = function() {
-    const iframe = document.getElementById('myWorkoutLogIframe');
-    if (iframe) {
-        iframe.src = 'https://ultimate-workout-log.kurtastarita.com'; // Set the iframe source after the listener is set up
-    }
-};
